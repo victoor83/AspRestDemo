611e6b3261383c27cfa4c8dd6d96a09a347fbe56
Paczynski <victoor@o2.pl>
2019-01-23 11:54:48 +0100

    init

---
+++ RestDemo/Controllers/ConfigController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestDemo.Controllers
+{
+    public class ConfigController : ApiController
+    {
+        // GET: api/Config
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Config/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Config
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Config/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Config/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
