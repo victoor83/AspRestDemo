7fb13f5b55d62654552118a6776db89f2e8a7600
Wik Pac <victoor@o2.pl>
2019-02-06 11:47:15 +0100

    updated

---
+++ RestDemo/DB_Model.edmx.sql
@@ -0,0 +1,53 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 01/24/2019 15:55:09
+-- Generated from EDMX file: D:\Programming\Repos\AspRestDemo\RestDemo\DB_Model.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [TestDB];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'People'
+CREATE TABLE [dbo].[People] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'People'
+ALTER TABLE [dbo].[People]
+ADD CONSTRAINT [PK_People]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ RestDemo/Scripts/MyScripts/HelloPage.js
@@ -0,0 +1,38 @@
+﻿var onButtonClick = function () {
+    $.ajax({  //ajax braucht man um backend function aufzurufen um z.B. etwas in 'DB' zu speichern (mit ajax ruft man Controller)
+        url: "/api/Values/" + $("#myinput1").val(),
+
+        success: function (result) {
+            $("#MyElement1").html("<strong>" + result + "</strong> ");
+        }
+    });
+}
+
+
+
+var onButtonClick2 = function () {
+    var person = prompt("Please enter your name", "Victor Paczo");
+    $("#MyElement1").html("<strong>" + person + "</strong> ");
+    window.alert("Your name is " + person);
+}
+
+
+$(document).ready(function ()
+{
+       var cuisines = ["Chinese", "Indian"];
+       var sel = document.getElementById('myCombo');
+       for (var i = 0; i < cuisines.length; i++) {
+           var opt = document.createElement('option');
+           opt.innerHTML = cuisines[i];
+           opt.value = cuisines[i];
+           sel.appendChild(opt);
+       }
+     // $.ajax({
+     //     url: "/api/Values/" + $("#myinput1").val(),
+
+     //     success: function (result)
+     //     {
+     //        $( "#MyElement1" ).html( "<strong>" + result + "</strong> " );
+     //     }
+     //});
+});
